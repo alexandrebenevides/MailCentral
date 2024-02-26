da37de5fe12bcfa5303e9443e0dbf29098a5a07c
Alexandre Benevides <alex.benevides@hotmail.com>
2024-02-25 23:13:10 -0300

    Enviando projeto base com Spring Boot configurado

---
+++ src/main/java/io/github/alexandrebenevides/mailcentral/MailCentral.java
@@ -0,0 +1,12 @@
+package io.github.alexandrebenevides.mailcentral;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MailCentral {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MailCentral.class, args);
+    }
+}
