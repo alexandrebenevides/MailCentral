7ca9867656863d0fd168e20e6757c5f8a61a24a6
Alexandre Benevides <alex.benevides@hotmail.com>
2024-02-26 01:28:34 -0300

    Configurando o banco de dados MySQL na aplicação

--- src/main/java/io/github/alexandrebenevides/mailcentral/MailCentral.java
+++ src/main/java/io/github/alexandrebenevides/mailcentral/MailCentral.java
@@ -2,10 +2,12 @@ package io.github.alexandrebenevides.mailcentral;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class MailCentral {
-
+    
     public static void main(String[] args) {
         SpringApplication.run(MailCentral.class, args);
     }
