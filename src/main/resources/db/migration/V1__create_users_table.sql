a3c2294fb9ccdbb2df433998d20d08858e54cb7c
Alexandre Benevides <alex.benevides@hotmail.com>
2024-02-27 23:01:50 -0300

    Adicionando e configurando o Flyway no projeto

---
+++ src/main/resources/db/migration/V1__create_users_table.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    full_name VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    password VARCHAR(100) NOT NULL,
+    created_at DATETIME NULL,
+    updated_at DATETIME NULL
+);
